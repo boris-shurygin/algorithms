61ca17aaa43cccec256fc85dc983edeec2d8bee1
Boris Shurygin <borisshr@gmail.com>
2015-04-18 23:58:14 +0300

    Fixed solution build. Unit tests running ok now

--- sources/UnitTest/utest_impl.h
+++ sources/UnitTest/utest_impl.h
@@ -2,8 +2,6 @@
  * File: utest_impl.h - Header for unit testing of libraries and tools of SCL (Simple Compiler)
  * Copyright (C) 2012  Boris Shurygin
  */
-#include "../Tools/tools_iface.h"
-#include "../opt/opt_iface.hpp"
-#include "../regexp/regexp_iface.h"
-#include "../syntax/stx_iface.h"
-#include "../lowering/low_iface.h"
+#include "../Utils/utils_iface.h"
+
+using namespace Utils;
