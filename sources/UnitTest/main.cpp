3da2ec914da3d370c7affd1d0571a38b55fdf754
Boris Shurygin <borisshr@gmail.com>
2015-04-18 13:41:36 +0300

    Added solution and UnitTest project from SCL

---
+++ sources/UnitTest/main.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file: UnitTest/main.cpp
+ * Implementation and entry point for unit testing of SCL (Simple Compiler)
+ */
+/*
+ * Copyright (C) 2012  Boris Shurygin
+ */
+#include "utest_impl.h"
+
+#undef UNIT_TEST_GUI
+
+/**
+ * The entry point for GUI version of SCL (Simple Compiler)
+ */
+int main(int argc, char **argv)
+{
+    Utils::MemMgr::init();
+
+    /** Test utils */
+    Utils::uTest();
+
+    /** Test Graph package */
+    RUN_TEST( Graph::uTest);
+    /** Test optimizer package */
+    RUN_TEST_OUT_FILE_CHECK( Opt::uTest, "opt_utest.txt");
+    
+    /*
+     * NOTE: For the unit tests below the logging can be usefull, 
+     * the logging itself is tested above in Utils::uTest
+     */
+    Utils::Log::init();
+    
+    //log()->add( LOG_REG_EXP, "RegExp", -1, "reg_exp_log.txt");
+    //log()->enable( LOG_REG_EXP);
+    
+    RUN_TEST( RegExp::uTestFA);
+    RUN_TEST( RegExp::uTest);
+    RUN_TEST_OUT_FILE_CHECK( Syntax::Arithmetics::uTest, "stx_utest.txt");
+    RUN_TEST_OUT_FILE_CHECK( Lowering::uTestArithm, "low_utest_arithm.txt");
+
+    Utils::Log::deinit();
+    
+
+    Utils::MemMgr::deinit();
+    
+    cout.flush();
+    cerr.flush();
+    Utils::TestDriver::printStats();
+    return Utils::TestDriver::returnRes();
+}
---
+++ sources/UnitTest/utest_impl.h
@@ -0,0 +1,9 @@
+/**
+ * File: utest_impl.h - Header for unit testing of libraries and tools of SCL (Simple Compiler)
+ * Copyright (C) 2012  Boris Shurygin
+ */
+#include "../Tools/tools_iface.h"
+#include "../opt/opt_iface.hpp"
+#include "../regexp/regexp_iface.h"
+#include "../syntax/stx_iface.h"
+#include "../lowering/low_iface.h"
