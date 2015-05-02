d696ca579964b4fd1bc06972a80d987f5cc13ecc
Boris Shurygin <borisshr@gmail.com>
2015-05-02 15:31:09 +0300

    slight changes in binSearch interface

--- sources/algorithms/alg_utest.cpp
+++ sources/algorithms/alg_utest.cpp
@@ -16,7 +16,7 @@ struct A
 };
 
 // Compare the 'b' field of a structure
-bool mycomp(A& a, A& b){ return a.b < b.b; }
+bool mycomp(const A& a, const A& b){ return a.b < b.b; }
 
 /**
  * Unit test for binary search
@@ -39,14 +39,13 @@ bool Alg::uTestBinSearch(UnitTest *utest_p)
     std::vector<A> data = { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 5 }, { 6, 5 }, { 5, 6 } };
     A item1 = { 0, 5 };
     A item2 = { 0, 4 };
-    A& item1_ref = item1;
-
+    
     // Test with custom comparison function (function pointer syntax)
     {
-        bool (*comp)(A& a, A& b);
+        bool (*comp)(const A& a, const A& b);
         comp = mycomp; //function pointer mess just to remind myself the syntax
 
-        auto res1 = binSearch(item1_ref, data, comp);
+        auto res1 = binSearch(item1, data, comp);
         auto res2 = binSearch(item1, data.begin(), data.end(), comp);
         auto res3 = binSearch(item2, data.begin(), data.end(), comp);
 
@@ -60,11 +59,11 @@ bool Alg::uTestBinSearch(UnitTest *utest_p)
         // Functor class
         struct Comp
         {
-            bool operator()(A& a, A& b) { return a.b < b.b; };
+            bool operator()(const A& a, const A& b) { return a.b < b.b; };
         };
         Comp comp;
 
-        auto res1 = binSearch(item1_ref, data, comp);
+        auto res1 = binSearch(item1, data, comp);
         auto res2 = binSearch(item1, data.begin(), data.end(), comp);
         auto res3 = binSearch(item2, data.begin(), data.end(), comp);
 
@@ -76,9 +75,9 @@ bool Alg::uTestBinSearch(UnitTest *utest_p)
     // Test with custom comparison function (lambda syntax)
     { 
         // Compare the 'b' field of a structure
-        auto comp = [](A& a, A& b) -> bool { return a.b < b.b; };
+        auto comp = [](const A& a, const A& b) -> bool { return a.b < b.b; };
         
-        auto res1 = binSearch(item1_ref, data, comp);
+        auto res1 = binSearch(item1, data, comp);
         auto res2 = binSearch(item1, data.begin(), data.end(), comp);
         auto res3 = binSearch(item2, data.begin(), data.end(), comp);
 
--- sources/algorithms/bin_search.h
+++ sources/algorithms/bin_search.h
@@ -17,7 +17,7 @@ namespace Alg
      * Binary search of element's iterator based on its value. Search is done between the 'from' and 'to' iterators
      */
     template <class Value, class RandomAccessIterator, class Compare = std::less<Value> >
-    typename RandomAccessIterator binSearch(Value val, RandomAccessIterator from, RandomAccessIterator to, Compare comp = std::less<Value>() )
+    typename RandomAccessIterator binSearch(const Value &val, RandomAccessIterator from, RandomAccessIterator to, Compare comp = std::less<Value>() )
     {
         auto not_found = to;
         while ( std::distance(from, to) >= 0)
@@ -43,7 +43,7 @@ namespace Alg
      * Returns iterator corresponding to the given value in the container
      */
     template <class Value, class Container, class Compare = std::less<Value> >
-    typename Container::iterator binSearch(Value val, Container &data, Compare comp = std::less<Value>())
+    typename Container::iterator binSearch(const Value &val, Container &data, Compare comp = std::less<Value>())
     {
         return binSearch(val, data.begin(), data.end(), comp);
     }
